2bef3f7821754d802781c6b209b8a4b49970b322
LHJJosh <91180701+LHJJosh@users.noreply.github.com>
2022-03-06 02:58:58 +0800

    Update SATSolver.java
    Latest version. Results seems to be accurate when ran with SATSolverTest.

--- 50001_Project-2D-starting/src/sat/SATSolver.java
+++ 50001_Project-2D-starting/src/sat/SATSolver.java
@@ -1,10 +1,8 @@
 package sat;
 
-import immutable.ImList;
-import sat.env.Environment;
-import sat.formula.Clause;
-import sat.formula.Formula;
-import sat.formula.Literal;
+import immutable.*;
+import sat.env.*;
+import sat.formula.*;
 
 /**
  * A simple DPLL SAT solver. See http://en.wikipedia.org/wiki/DPLL_algorithm
@@ -15,19 +13,21 @@ public class SATSolver {
      * unit propagation. The returned environment binds literals of class
      * bool.Variable rather than the special literals used in clausification of
      * class clausal.Literal, so that clients can more readily use it.
-     * 
+     *
      * @return an environment for which the problem evaluates to Bool.TRUE, or
      *         null if no such environment exists.
      */
-    public static Environment solve(Formula formula) {
-        // TODO: implement this.
-        throw new RuntimeException("not yet implemented.");
+    public static Environment solve(Formula formula)
+    {
+        Environment newEnv = new Environment(); //environment
+
+        return solve(formula.getClauses(), newEnv); //recursion
     }
 
     /**
      * Takes a partial assignment of variables to values, and recursively
      * searches for a complete satisfying assignment.
-     * 
+     *
      * @param clauses
      *            formula in conjunctive normal form
      * @param env
@@ -37,14 +37,65 @@ public class SATSolver {
      *         or null if no such environment exists.
      */
     private static Environment solve(ImList<Clause> clauses, Environment env) {
-        // TODO: implement this.
-        throw new RuntimeException("not yet implemented.");
+        // find smallest clause
+        // if number of literals = 1, call substitute()
+        // else use the code here
+        // choose a literal here, set it to be TRUE
+        if (clauses.isEmpty()) //check if variable has a truth value
+        {
+            return env; //return SAT if there is none
+        }
+        // smallest clause
+        Clause smallest = clauses.first();
+        for (Clause c: clauses)
+        {
+            if (c.isEmpty())
+            {
+                return null;
+            }
+            else if (c.size() < smallest.size())
+            {
+                smallest = c;
+            }
+        }
+        Literal l = smallest.chooseLiteral();
+        Variable v = l.getVariable();
+        //System.out.println(v.toString());
+        if (smallest.isUnit())
+        {
+            Bool b;
+            if (l instanceof PosLiteral)
+            {
+                b = Bool.TRUE;
+            }
+            else
+            {
+                b = Bool.FALSE;
+            }
+            env = env.put(v, b);
+            //System.out.println(smallest.toString());
+            return solve(substitute(clauses, l), env);
+        }
+        else
+        {
+            if (l instanceof NegLiteral)
+            {
+                l = l.getNegation();
+            }
+            Environment newenv = solve(substitute(clauses, l),env.put(v, Bool.TRUE));
+            //System.out.println(smallest.toString());
+            if (newenv == null)
+            {
+                return solve(substitute(clauses, l.getNegation()),env.put(v, Bool.FALSE));
+            }
+            return newenv;
+        }
     }
 
     /**
      * given a clause list and literal, produce a new list resulting from
      * setting that literal to true
-     * 
+     *
      * @param clauses
      *            , a list of clauses
      * @param l
@@ -52,9 +103,21 @@ public class SATSolver {
      * @return a new list of clauses resulting from setting l to true
      */
     private static ImList<Clause> substitute(ImList<Clause> clauses,
-            Literal l) {
-        // TODO: implement this.
-        throw new RuntimeException("not yet implemented.");
+                                             Literal l) {
+        ImList<Clause> substituted = new EmptyImList<Clause>();
+        for (Clause c : clauses)
+        {
+            Clause newc = c.reduce(l);
+            if (newc != null)
+            {
+                substituted = substituted.add(newc);
+            }
+        }
+        return substituted;
+        // use if number of literals in a clause = 1
+        // assign that literal value for the entire list
+        // eliminate all the other blanks that are not literals
+        // call solve
     }
 
 }
