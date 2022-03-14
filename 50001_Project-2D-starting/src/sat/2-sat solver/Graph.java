2a90f098265dfa36551dd13d329e9dfad1cdfd45
Han Wei Guang <hanweiguang25@gmail.com>
2022-03-14 14:26:43 +0800

    added graph class

---
+++ 50001_Project-2D-starting/src/sat/2-sat solver/Graph.java
@@ -0,0 +1,15 @@
+
+public class Graph {
+    private int V; // no of vertices
+    private LinkedList <Integer>  adj[]; //Adjacency list
+
+    Graph(){
+        V = v;
+        adj_list = new LinkedList[v];
+        for (int i = 0; i < v ; i++){
+            adj[i] = new LinkedList();
+        }
+
+    }
+}
+
